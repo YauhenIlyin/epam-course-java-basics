fcc38dd7e00c93ade55e514c6652c27731252cfc
Yauhen Ilyin <prog.jekylin@gmail.com>
2021-03-27 02:58:31 +0300

    little fix in test

--- src/test/java/by/ilyineugene/arrayapp/service/ArrayElementCalculatorTest.java
+++ src/test/java/by/ilyineugene/arrayapp/service/ArrayElementCalculatorTest.java
@@ -17,16 +17,37 @@ public class ArrayElementCalculatorTest {
         arrayElementCalculator = new ArrayElementCalculator();
     }
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         arrayElementCalculator = null;
         entityArray = null;
     }
-    @DataProvider(name = "Double date")
-    public Object[][] createData() {
-        return new Object[][]{{new Double[]{3.5, 4.5, 4.0},4.0},{new Double[]{1.0, 2.0, 6.0},3.0}};
+    @DataProvider(name = "Double data")
+    public Object[][] createDoubleData() {
+        return new Object[][]{
+                { new Double[]{3.5, 4.5, 4.},4. },
+                { new Double[]{1., 2., 6.},3. },
+                { new Double[]{0., 0., 0.},0 },
+                { new Double[]{-1.3, -0.7, -1.},-1. }
+        };
     }
-    @Test(dataProvider = "Double date")
+    @Test(dataProvider = "Double data")
     public void calculateAverageValue(Double [] array, Double expected) {
+        EntityArray<Double> entityArray = new EntityArray<>(array);
+        Double actual = arrayElementCalculator.calculateAverageValue(entityArray.getArray());
+        assertEquals(actual.doubleValue(), expected.doubleValue(), 0.001, "AverageValue of array test failure...");
+    }
+
+    @DataProvider(name = "Integer data")
+    public Object[][] createIntegerData() {
+        return new Object[][]{
+                { new Double[]{1, 2, 3},2 },
+                { new Double[]{1., 2., 6.},3. },
+                { new Double[]{0., 0., 0.},0 },
+                { new Double[]{-1.3, -0.7, -1.},-1. }
+        };
+    }
+    @Test(dataProvider = "Integer data")
+    public void calculateAverageValue(Integer [] array, Integer expected) {
         EntityArray<Double> entityArray = new EntityArray<>(array);
         Double actual = arrayElementCalculator.calculateAverageValue(entityArray.getArray());
         assertEquals(actual.doubleValue(), expected.doubleValue(), 0.001, "AverageValue of array test failure...");
